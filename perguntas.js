c546d0343d3fc399e20f8837dcecc3407773b659
PIDPALA <130073782+PIDPALA@users.noreply.github.com>
2024-10-21 09:54:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'historia',
+    'Qual o tamanho da quadra?',
+    '28 metros de comprimento por 15 metros de largura',
+
+)
+
+criaCartao(
+    'historia',
+    'Qual e o maior jogador de basquete em titúlos do mundo?',
+    'Michael Jordan',
+
+)
+
+criaCarta(
+    'hitoria',
+    'Qual equipe tem mais campeonatos da NBA na historia?',
+    'Boston Celtics e a equipe com mais títulos da NBA',
+)
+
